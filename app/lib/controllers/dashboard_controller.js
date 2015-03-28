ececf31f696be7ff385641220a559536b1c4c644
Silviu D. Bojica <silviu@cubicleapps.com>
2015-03-28 20:42:44 +0400

    Refactor home to dashboard

---
+++ app/lib/controllers/dashboard_controller.js
@@ -0,0 +1,10 @@
+DashboardController = RouteController.extend({
+  layoutTemplate: 'MasterLayout',
+
+  // subscriptions: function() {
+  // },
+
+  action: function() {
+    this.render('Dashboard');
+  }
+});
